8b86e8e1304bb5616d7da2be290fc693b0d6b9c1
Mykhailo <mcmikius@icloud.com>
2019-08-27 20:43:46 +0300

    Get to Know the Sample Data

--- Landmarks/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
@@ -37,9 +37,9 @@ struct ContentView: View {
 }
 
 #if DEBUG
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
 #endif
