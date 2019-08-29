b406c9f9af63768c3a3aebb4c962aa000a160920
Mykhailo <mcmikius@icloud.com>
2019-08-29 13:56:08 +0300

    init

---
+++ StartingPoint/Landmarks/Landmarks/ LandmarkRow.swift
@@ -0,0 +1,24 @@
+//
+//   LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Mykhailo Bondarenko on 29.08.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct _LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        _LandmarkRow()
+    }
+}
