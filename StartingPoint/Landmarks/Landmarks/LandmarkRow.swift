c425b4f9cf315d431837cc5bbae182db39cf50ba
Mykhailo <mcmikius@icloud.com>
2019-08-30 10:25:47 +0300

    Mark the User’s Favorite Landmarks

--- StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
+++ StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
@@ -18,6 +18,12 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+            Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -28,7 +34,7 @@ struct LandmarkRow_Previews: PreviewProvider {
             LandmarkRow(landmark: landmarkData[0])
             LandmarkRow(landmark: landmarkData[1])
         }
-
+            
         .previewLayout(.fixed(width: 300, height: 70))
     }
 }
