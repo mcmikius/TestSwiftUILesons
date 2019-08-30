8b6d029b2e98bd683c48be6ec7f58cbf3b9fd3c2
Mykhailo <mcmikius@icloud.com>
2019-08-30 09:19:09 +0300

    Create the List of Landmarks

---
+++ StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mykhailo Bondarenko on 30.08.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
