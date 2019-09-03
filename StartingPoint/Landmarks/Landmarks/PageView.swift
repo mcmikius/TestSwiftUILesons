1fa620f1a33328b0e49856e2e017a5efc3f95a40
Mykhailo <mcmikius@icloud.com>
2019-09-03 13:39:58 +0300

    Create View to Represent a UIPageViewController

---
+++ StartingPoint/Landmarks/Landmarks/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Mykhailo Bondarenko on 03.09.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+
+    var body: some View {
+        PageViewController(controllers: viewControllers)
+    }
+}
+
+struct PageView_Preview: PreviewProvider {
+    static var previews: some View {
+        PageView(features.map { FeatureCard(landmark: $0) })
+        .aspectRatio(3/2, contentMode: .fit)
+    }
+}
