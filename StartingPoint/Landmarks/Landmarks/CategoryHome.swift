3a98ce92f7aaa09cab02085905540b3f765bf05b
Mykhailo <mcmikius@icloud.com>
2019-09-03 07:53:50 +0300

    Add a Home View

---
+++ StartingPoint/Landmarks/Landmarks/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mykhailo Bondarenko on 03.09.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Landmarks Content")
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
