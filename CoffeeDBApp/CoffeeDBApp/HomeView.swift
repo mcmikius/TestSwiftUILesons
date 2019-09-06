e53d61394f454b5ee3711abcbbe3aaaac9e7aba7
Mykhailo <mcmikius@icloud.com>
2019-09-06 10:02:59 +0300

    Add HomeView

--- CoffeeDBApp/CoffeeDBApp/HomeView.swift
+++ CoffeeDBApp/CoffeeDBApp/HomeView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    var categories: [String: [Drink]] {
+        .init(
+            grouping: drinkData, by: {$0.category.rawValue}
+        )
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(categories.keys.sorted(), id: \String.self) { key in
+                DrinkRow(categoryName: "\(key) Drinks".uppercased(), drinks: self.categories[key]!)
+                    .frame(height: 320)
+                    .padding(.top)
+                    .padding(.bottom)
+            }
+            .navigationBarTitle(Text("COFFEE DB"))
+        }
     }
 }
 
