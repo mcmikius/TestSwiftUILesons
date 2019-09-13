8223b52480587dcccde35bd5612bf961280ff569
Mykhailo <mcmikius@icloud.com>
2019-09-13 09:57:23 +0300

    Getting Started with SwiftUI

---
+++ Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Mykhailo Bondarenko on 13.09.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to my first app")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
