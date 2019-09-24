568d4a2c3f1e2b18b8870835db611411c54530b1
Mykhailo <mcmikius@icloud.com>
2019-09-24 13:09:18 +0300

    Done

---
+++ SwiftUICoreData/SwiftUICoreData/AddNewPersonView.swift
@@ -0,0 +1,50 @@
+//
+//  AddNewPersonView.swift
+//  SwiftUICoreData
+//
+//  Created by Mykhailo Bondarenko on 24.09.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddNewPersonView: View {
+    
+    @State private var addImage = false
+    @Binding var image: UIImage?
+    @Binding var firstName: String
+    @Binding var lastName: String
+    @Binding var age: String
+    @Binding var segmentedControl: Int
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        VStack {
+            Image(uiImage: image ?? UIImage(systemName: "plus.circle")!).resizable().scaledToFit().clipShape(Circle()).padding().shadow(color: .secondary, radius: 7).onTapGesture {
+                self.addImage.toggle()
+            }
+            VStack {
+                TextField("First name", text: self.$firstName).textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Last name", text: self.$lastName).textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Age", text: self.$age).textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            Picker(selection: self.$segmentedControl, label: Text("")) {
+                Text("Male")
+                Text("Female")
+            }.padding().shadow(color: .secondary, radius: 3).pickerStyle(SegmentedPickerStyle())
+            Image(systemName: self.isFavorite ? "star.fill" : "star").resizable().frame(width: 50, height: 50).foregroundColor(.yellow).shadow(color: self.isFavorite ? .primary : .secondary, radius: 3).onTapGesture {
+                self.isFavorite.toggle()
+            }
+            Spacer()
+            
+        }.sheet(isPresented: self.$addImage) {
+            ImagePickerView(showImagePicker: self.$addImage, image: self.$image)
+        }.padding()
+    }
+}
+
+struct AddNewPersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewPersonView(image: .constant(UIImage(systemName: "plus.circle")), firstName: .constant("John"), lastName: .constant("Dou"), age: .constant("..."), segmentedControl: .constant(0), isFavorite: .constant(true))
+    }
+}
--- SwiftUICoreData/SwiftUICoreData/ContentView.swift
+++ SwiftUICoreData/SwiftUICoreData/ContentView.swift
@@ -9,8 +9,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(fetchRequest: Person.fetchRequest()) var persons: FetchedResults<Person>
+    
+    @State private var addImage = false
+    @State private var addPerson = false
+    @State private var image: UIImage?
+    @State private var isFavorite = false
+    @State private var selectedSegment = 0
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var age = ""
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                HStack(alignment: .center, spacing: 25.0) {
+                    Text("Add new person").font(.headline)
+                    Button(action: {
+                        self.addPerson.toggle()
+                    }, label: {
+                        Image(systemName: "plus.circle.fill").foregroundColor(.green).imageScale(.large)
+                    })
+                }
+                List {
+                    ForEach(persons, id: \.id) { person in
+                        PersonCellView(image: Image(uiImage: UIImage(data: person.photoData!) ?? UIImage(systemName: "person")!), favoriteImage: Image(systemName: person.isFavorite ? "star.fill" : "star"), gender: person.gender ?? "...", age: person.age ?? "...", firstName: person.firstName ?? "...", lastName: person.lastName ?? "...")
+                    }.onDelete { IndexSet in
+                        guard 0 < self.persons.count else { return }
+                        self.managedObjectContext.delete(self.persons[IndexSet.first!])
+                        do {
+                            try self.managedObjectContext.save()
+                        } catch {
+                            print(error, error.localizedDescription)
+                        }
+                    }
+                }
+            }.sheet(isPresented: self.$addPerson, onDismiss: {
+                guard !self.firstName.isEmpty, !self.lastName.isEmpty, !self.age.isEmpty else { return }
+                let personManagedObjectContext = Person(context: self.managedObjectContext)
+                personManagedObjectContext.firstName = self.firstName
+                personManagedObjectContext.lastName = self.lastName
+                personManagedObjectContext.age = self.age
+                
+                if self.selectedSegment == 0 {
+                    personManagedObjectContext.gender = "male"
+                } else {
+                    personManagedObjectContext.gender = "female"
+                }
+                
+                personManagedObjectContext.isFavorite = self.isFavorite
+                personManagedObjectContext.photoData = self.image?.pngData()
+                
+                do {
+                    try self.managedObjectContext.save()
+                } catch {
+                    print(error, error.localizedDescription)
+                }
+                
+                self.isFavorite = false
+                self.firstName = ""
+                self.lastName = ""
+                self.age = ""
+                self.image = UIImage(systemName: "plus.circle")
+                
+            }) {
+                AddNewPersonView(image: self.$image, firstName: self.$firstName, lastName: self.$lastName, age: self.$age, segmentedControl: self.$selectedSegment, isFavorite: self.$isFavorite)
+                }.navigationBarTitle(Text("Persons")).navigationBarItems(trailing: EditButton())
+        }
+        
     }
 }
 
