68b14d2c27af4618650e894cf8463417008b09f6
Mykhailo <mcmikius@icloud.com>
2019-09-26 14:46:06 +0300

    Fetching Categories

--- CategoricalExpenses/CategoricalExpenses/CategoriesViewController.swift
+++ CategoricalExpenses/CategoricalExpenses/CategoriesViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriesViewController: UIViewController {
 
     @IBOutlet weak var categoriesTableView: UITableView!
     
+    var categories: [Category] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try managedContext.fetch(fetchRequest)
+            categoriesTableView.reloadData()
+        } catch {
+            print("Could not fetch")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,11 +42,13 @@ class CategoriesViewController: UIViewController {
 
 extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = categoriesTableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.title
         
         return cell
     }
