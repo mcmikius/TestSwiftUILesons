dbe1d4bc298b20776c0a928271e77aa60f6760fa
Mykhailo <mcmikius@icloud.com>
2019-09-30 13:16:28 +0300

    Getting started

---
+++ PetPal/PetPal/Friend+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Friend+CoreDataClass.swift
+//  PetPal
+//
+//  Created by Mykhailo Bondarenko on 30.09.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Friend: NSManagedObject {
+
+}
---
+++ PetPal/PetPal/Friend+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Friend+CoreDataProperties.swift
+//  PetPal
+//
+//  Created by Mykhailo Bondarenko on 30.09.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Friend {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Friend> {
+        return NSFetchRequest<Friend>(entityName: "Friend")
+    }
+
+    @NSManaged public var name: String?
+
+}
