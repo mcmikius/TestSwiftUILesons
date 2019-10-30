6de4f7b7db2767d88ee905dd5cc67e6dd83cd30f
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-10-30 21:41:59 +0200

    something wrong with wiliest didst

--- UnsplashClient/UnsplashClient/ContentView.swift
+++ UnsplashClient/UnsplashClient/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var store: UnsplashStore
     var body: some View {
-        Text("Hello World")
+        Group {
+            if store.models.isEmpty {
+                LoadingView()
+            } else {
+                NavigationView {
+                    List(store.models, rowContent: RowView.init).navigationBarTitle(Text("Photographers"))
+                }
+            }
+        }.onAppear(perform: store.fetch)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: UnsplashStore())
+    }
+}
+
+struct RowView: View {
+    let model: Model
+    var body: some View {
+        return HStack{
+            Image(uiImage: model.image)
+                .frame(width: 50.0, height: 50)
+                .scaledToFit().shadow(radius: 10)
+            VStack(alignment: .leading) {
+                Text(model.name)
+                Text(model.bio)
+                    .font(.subheadline).foregroundColor(.secondary)
+            }
+        }
     }
 }
---
+++ UnsplashClient/UnsplashClient/UnsplashStore.swift
@@ -0,0 +1,36 @@
+//
+//  UnsplashStore.swift
+//  UnsplashClient
+//
+//  Created by Mykhailo Bondarenko on 30.10.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class UnsplashStore: ObservableObject {
+    static let url = URL(string: "https://api.unsplash.com/photos?client_id=d30dd0af646aedfa6e85f6ace11282e8b2086a582d456a3608459e411865fe32")!
+    var models: [Model] = [] {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    func fetch() {
+        URLSession.shared.dataTask(with: UnsplashStore.url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+                
+            }
+            guard let models = try? Unsplash(data: data) else {
+                return
+            }
+            let viewModels = models.map { $0.user }.compactMap(Model.init)
+            DispatchQueue.main.async {
+                self.models = viewModels
+            }
+        }.resume()
+    }
+}
