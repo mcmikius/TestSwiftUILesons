a43fc2048899b2af6b6fc2688b9f7d54a0dff241
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-11-01 14:57:07 +0200

    Setting Up Backend Server

--- Crypto/Crypto/LoginView.swift
+++ Crypto/Crypto/LoginView.swift
@@ -7,21 +7,61 @@
 //
 
 import SwiftUI
+import Combine
+
+struct ServerMessage: Decodable {
+    let status, message: String
+}
+
+class HttpAuth: ObservableObject {
+    let objectWillChange = PassthroughSubject<HttpAuth, Never>()
+    var authenticated = false {
+        willSet {
+            objectWillChange.send(self)
+        }
+    }
+    func checkDetails(username: String, password: String) {
+        guard let url = URL(string: "http://localhost/...") else { return }
+        let body = ["username": username, "password": password]
+        let finalBody = try! JSONSerialization.data(withJSONObject: body)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalBody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            let finalData = try! JSONDecoder().decode(ServerMessage.self, from: data)
+            if finalData.status == "ok" {
+                DispatchQueue.main.async {
+                    self.authenticated = true
+                }
+            }
+        }.resume()
+    }
+}
 
 struct LoginView: View {
     
     @State private var username: String = ""
     @State private var password: String = ""
     
+    @State private var manager = HttpAuth()
+    
     var body: some View {
         NavigationView {
             Form {
                 TextField("Your username", text: $username)
                 SecureField("Your Passwird", text: $password)
+                
+                if manager.authenticated {
+                    Text("You are logged in!")
+                }
+                
                 HStack {
                     Spacer()
                     Button(action: {
-                        print("Login")
+                        self.manager.checkDetails(username: self.username, password: self.password)
                     }) {
                         Text("Login")
                             .foregroundColor(Color.white).padding(.horizontal, 20).padding([.top, .bottom], 15)
