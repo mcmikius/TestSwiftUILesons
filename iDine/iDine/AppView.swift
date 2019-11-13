3c1f1a437f18ec6187ce812ec8df26b7f118dac8
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-11-13 14:05:18 +0200

    Adding TabView and tabItem()

---
+++ iDine/iDine/AppView.swift
@@ -0,0 +1,31 @@
+//
+//  AppView.swift
+//  iDine
+//
+//  Created by Mykhailo Bondarenko on 13.11.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            ContentView().tabItem {
+                Image(systemName: "list.dash")
+                Text("Menu")
+            }
+            OrderView().tabItem {
+                Image(systemName: "square.and.pencil")
+                Text("Order")
+            }
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        AppView().environmentObject(order)
+    }
+}
