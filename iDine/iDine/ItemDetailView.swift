4e04067a81bc70529429ce1f09959e27dd5ddfa1
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-11-13 14:00:00 +0200

    Adding items to an order with @EnvironmentObject

--- iDine/iDine/ItemDetailView.swift
+++ iDine/iDine/ItemDetailView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct ItemDetailView: View {
+    @EnvironmentObject var order: Order
     var item: MenuItem
     var body: some View {
         VStack {
@@ -17,15 +18,19 @@ struct ItemDetailView: View {
                 Text("Photo: \(item.photoCredit)").padding(4).background(Color.black).font(.caption).foregroundColor(.white).offset(x: -5, y: -5)
             }
             Text(item.description).padding()
+            Button("Order This"){
+                self.order.add(item: self.item)
+            }.font(.headline)
             Spacer()
         }.navigationBarTitle(Text(item.name), displayMode: .inline)
     }
 }
 
 struct ItemDetailView_Previews: PreviewProvider {
+    static let order = Order()
     static var previews: some View {
         NavigationView {
-            ItemDetailView(item: MenuItem.example)
+            ItemDetailView(item: MenuItem.example).environmentObject(order)
         }
     }
 }
---
+++ iDine/iDine/OrderView.swift
@@ -0,0 +1,40 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Mykhailo Bondarenko on 13.11.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: Text("Check out")) {
+                        Text("Place Order")
+                    }
+                }
+                }.navigationBarTitle("Order").listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
